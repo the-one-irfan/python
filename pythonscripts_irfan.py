399597198a3e7e70b5e3a6452aeeaeb63de7b117
the-one-irfan <74822418+the-one-irfan@users.noreply.github.com>
2020-11-21 05:23:47 -0800

    Add files via upload

---
+++ pythonscripts_irfan.py
@@ -0,0 +1,30 @@
+#data types
+#integers- whole numbers wiyhout decimal place.
+##45 #integer
+#True False #booleans
+
+#print("hello world")
+
+#var1= int(input("enter first number"))
+#var2=int(input("enter second number"))
+
+#1result= var1 * var2
+
+#print(result)
+#print('this is ade\'s  pen')
+
+print(' "rome was not built in a day"- Abraham Lincoln')
+
+print('''
+pen
+books
+uniform
+''')
+
+print("photo" + "synthesis")
+
+num=232
+print(float(num))
+
+
+print ("my number is " + str(num))
